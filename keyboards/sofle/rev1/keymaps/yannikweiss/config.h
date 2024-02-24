1b9387b150aff77bb160f73b6f3aba3460a158a3
Yannik Weiß <yannik@magicsmoke.dev>
2024-02-24 23:35:50 +0100

    First try

---
+++ keyboards/sofle/rev1/keymaps/yannikweiss/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#undef MANUFACTURER
+#define MANUFACTURER "Yannik Weiß"
+
+#undef ENCODER_RESOLUTION
+#define ENCODER_RESOLUTION 4
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
